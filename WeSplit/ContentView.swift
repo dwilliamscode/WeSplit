68b7a095765af0613fe85fd1acfcdfccc27bf590
dwilliamscode <Demetrius.Williams.PMP@gmail.com>
2020-05-25 02:25:29 -0400

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by dw on 5/25/20.
+//  Copyright © 2020 WilliamsAssociates. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
